4496b737577d763b4888064783e0116ad60fd4cc
Patrick Canfield <pscale01@gmail.com>
2020-01-17 20:31:13 -0800

    Javascript/CSS for upload page

--- posts/static/upload.js
+++ posts/static/upload.js
@@ -1,23 +1,87 @@
+let uploadInProgress = false;
+
+const imageInput = document
+  .querySelector("#image-input");
+
+loadImageSrcFromInput(imageInput);
+
+imageInput
+  .addEventListener("change", handleChangeFile);
+
+function handleChangeFile(event) {
+  loadImageSrcFromInput(event.target)
+  submitButton.className = decideClassListForSubmitButton();
+  submitButton.setAttribute("value", decideValueForSubmitButton());
+}
+
+function loadImageSrcFromInput(input) {
+  if(input.files[0]) {
+    loadImageSrc(input.files[0], setPreviewImageSrc)
+  }
+}
+
+function loadImageSrc(file, callback) {
+  const reader = new FileReader();
+
+  const handleLoad = (event) => {
+    callback(event.target.result);
+    reader.removeEventListener("load", handleLoad);
+  }
+
+  reader.addEventListener("load", handleLoad)
+  reader.readAsDataURL(file)
+}
+
+const previewImage = document.querySelector("#preview-image");
+
+function setPreviewImageSrc(src) {
+  previewImage.setAttribute("src", src);
+  previewImage.className = "Preview Preview__in";
+}
+
+const submitButton = document.querySelector("#submit-button");
+
+submitButton.className = decideClassListForSubmitButton();
+submitButton.setAttribute("value", decideValueForSubmitButton());
+
+function decideClassListForSubmitButton() {
+  return imageInput.files[0] ? "Button mb" : "hidden";
+}
+
+function decideValueForSubmitButton() {
+  return uploadInProgress ? "Uploading…" : imageInput.files[0] ? `Upload ${imageInput.files[0].name}` : "";
+}
 
 document
   .querySelector("#upload-form")
-  .addEventListener("submit", listener);
+  .addEventListener("submit", handleSubmit);
 
-function listener(event) {
+const scanner = document.querySelector("#scanner")
+
+function handleSubmit(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
+
+  scanner.style.setProperty("--scanner-is-on", '1');
+
+  if(uploadInProgress) {
+    return;
+  }
+
+  uploadInProgress = true;
+  submitButton.setAttribute("disabled", true);
+  submitButton.setAttribute("value", decideValueForSubmitButton());
 
   axios.request( {
     method: "post",
     url: "/api/upload",
     data: formData,
-    onUploadProgress: (p) => {
-      console.log(p);
+    onUploadProgress: (event) => {
+      const progress = `${100 * (event.loaded / event.total)}%`;
+      scanner.style.setProperty("--scanner-progress", progress);
     }
-  }).then (data => {
-    console.log(data);
-    //this.setState({
-    //fileprogress: 1.0,
-    //})
+  }).then (() => {
+      scanner.style.setProperty("--scanner-progress", '100%');
+  scanner.style.setProperty("--scanner-is-on", '0');
   })
 }
