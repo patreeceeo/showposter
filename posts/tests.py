f91868bb9d4e73d56f2a604cbdd36bf17915a555
Patrick Canfield <pscale01@gmail.com>
2020-01-20 14:22:03 -0800

    clean up test

--- posts/tests.py
+++ posts/tests.py
@@ -4,12 +4,11 @@ from posts.models import Post
 
 class PostModelTestCase(TestCase):
     def test_slug(self):
-        """should auto assign slug based on number of existing posts"""
+        """should auto assign unique slug"""
         post = Post.objects.create(
                 end_date='2066-06-06',
                 alternate_text='woohoo!'
         )
-        print('slug=%s' % post.slug)
         self.assertIsNot(post.slug, '');
         self.assertTrue(type(post.slug) is str);
 
